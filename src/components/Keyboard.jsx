dc004e46be18c59661ca8829f5f562e7dfa09b22
Glitch (glitch-hello-react) <none>
2022-02-04 20:11:31 +0000

    🍿🔮 Checkpoint ./src/components/Keyboard.jsx:6959340/12689 ./src/styles/theme.js:6959340/1236

--- src/components/Keyboard.jsx
+++ src/components/Keyboard.jsx
@@ -3,15 +3,56 @@ import styled from "styled-components";
 
 const KeyboardContainer = styled.div`
   height: 200px;
-  background-color: grey;
   margin: 0;
   user-select: none;
 `;
 
-const KEYS = [['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p'], ['a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l'], ['enter', 'z', 'x', 'c', 'v', 'b', 'n', 'm']]
+const Row = styled.div`
+  display: flex;
+  width: 100%;
+  margin: 0 auto 8px;
+  touch-action: manipulation;
+`;
+
+const Key = styled.button`
+  font-weight: bold;
+  border: 0;
+  padding: 0;
+  margin: 0 6px 0 0;
+  height: 58px;
+  border-radius: 4px;
+  cursor: pointer;
+  user-select: none;
+  background-color: ${(props) => props.theme.keyBackgroundColor};
+  color: ${(props) => props.theme.keyTextColor};
+  tap-highlight-color: ${(props) => props.theme.keyHighlightColor};
+  flex: 1;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-transform: uppercase;
+`;
+
+const KEYS = [
+  ["q", "w", "e", "r", "t", "y", "u", "i", "o", "p"],
+  ["a", "s", "d", "f", "g", "h", "j", "k", "l"],
+  ["enter", "z", "x", "c", "v", "b", "n", "m", "back"],
+];
 
 export const Keyboard = () => {
-  return <KeyboardContainer />;
+  return (
+    <KeyboardContainer>
+      {KEYS.map((row, index) => {
+        return (
+          <Row key={"row" + index}>
+            {row.map((key) => {
+              return <Key key={key}>{key}</Key>;
+            })}
+          </Row>
+        );
+      })}
+    </KeyboardContainer>
+  );
 };
 
 export default Keyboard;
--- src/styles/theme.js
+++ src/styles/theme.js
@@ -2,4 +2,7 @@ export const theme = {
   backgroundColor: "#121213",
   textColor: "#d7dadc",
   borderColor: "#3a3a3c",
+  keyBackgroundColor: "#818384",
+  keyTextColor: "#d7dadc",
+  keyHighlightColor: "#0000004d",
 };
