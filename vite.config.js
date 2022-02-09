94f57308c10c5d4976ea27fa29aa793b1d67f93a
Glitch (glitch-hello-react) <none>
2022-02-09 01:18:21 +0000

    🐣🚎 Checkpoint ./src/icons/back.svg:6959340/1172 ./src/components/Keyboard.jsx:6959340/13215 ./package.json:6959340/3320 ./vite.config.js:6959340/1216

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,17 @@
 import { defineConfig } from "vite";
 import reactRefresh from "@vitejs/plugin-react-refresh";
+import svgr from '@honkhonk/vite-plugin-svgr'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [reactRefresh()],
+  plugins: [reactRefresh(), svgr()],
   build: {
-    outDir: "build"
+    outDir: "build",
   },
   server: {
     strictPort: true,
     hmr: {
-      port: 443 // Run the websocket server on the SSL port
-    }
-  }
+      port: 443, // Run the websocket server on the SSL port
+    },
+  },
 });
