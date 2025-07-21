6fafdb5ef606fd9f6181811a6264a5a1c44beb67
Glitch (glitch-hello-react) <none>
2025-07-21 17:11:46 -0400

    Remove glitch references

--- src/static/globals.js
+++ src/static/globals.js
@@ -1,4 +1,4 @@
 export const TITLE = "Jordle";
-export const URL = "https://jordle.glitch.me";
+export const URL = "https://jordle-game.vercel.app";
 export const WORD_LENGTH = 5;
 export const NUM_GUESSES = 6;
